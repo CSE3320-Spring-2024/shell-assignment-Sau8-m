89b1aab421df505a2e26dfb67b4028c8f2b81a06
Sau8-m <114531767+Sau8-m@users.noreply.github.com>
2024-02-14 02:37:59 +0000

    First

--- msh/msh.c
+++ msh/msh.c
@@ -30,9 +30,116 @@
 #include <errno.h>
 #include <string.h>
 
+#define MAX_NUM_ARGUMENTS 5
+#define MAX_COMMAND_SIZE 255
+#define WHITESPACE "\t \n"
 
-int main( int argc, char * argv[] )
+void exe(char *argv[]);
+
+int main( int argc, char *argv[] )
 {
+    char *command_string = (char*)malloc(MAX_COMMAND_SIZE);
+    char error_message[30] = "An error has occurred\n";
+  
+  while(1)
+  {
+    printf("msh> ");
+    
+    while (!fgets(command_string,MAX_COMMAND_SIZE,stdin));
+  
+    char *token[MAX_NUM_ARGUMENTS];
+    int token_count =0;
+    char *argument_ptr;
+    char *working_string = strdup(command_string);
+    char *head_prt = working_string;
+
+    while(((argument_ptr= strsep(&working_string,WHITESPACE)) !=NULL) && (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup(argument_ptr,MAX_COMMAND_SIZE);
+      if(strlen(token[token_count]) == 0)
+      {
+        token[token_count] =NULL;
+      }
+      token_count++;
+    }
+    for(int i =0 ; i<token_count;i++)
+    {
+        printf("%d   %s\n",i, token[i]);
+    }
+    
+   if (token[0] && strcmp(token[0], "exit") == 0)
+   {
+        if(token[1] != NULL)
+        {
+            write(STDERR_FILENO,error_message,strlen(error_message));
+        }
+        else
+            exit(0);
+   }
+   else if (token[0] && strcmp(token[0], "pwd")==0)
+   {
+    pid_t pid = fork();
+        if(pid ==0)
+        {
+            printf("%s\n",getcwd(token[1],50));
+        }
+        else{
+            int status;
+            wait(&status);
+        }
+   }
+   else if(token[0] && strcmp(token[0],"ls")==0)
+   {
+        exe(token);
+   }
+   else if(token[0] && strcmp(token[0],"cd")==0)
+   {
+    pid_t pid = fork();
+    
+        if(pid==0)
+        {
+            if( token[1] == NULL || token[2] != NULL)
+            {
+                write(STDERR_FILENO,error_message,strlen(error_message));
+            }
+            else 
+            {
+                chdir(token[1]);
+                printf("%s\n",getcwd(token[1],50));
+            }
+        }
+        else{
+            int status;
+            wait(&status);
+        }
+    
+   }
+   
+   free(head_prt);
+  }
+  
   return 0;
+}
+
+void exe(char *argv[])
+{
+    pid_t pid = fork();
+    if(pid ==0)
+    {
+        if(access("/bin/ls",X_OK)==0)
+        {
+            int ret = execvp(argv[0], argv);
+            if(ret == -1)
+            {
+                perror("execfail");
+            }
+        }
+
+    }
+    else{
+        int status;
+        wait(&status);
+    }
+    
 }
 
